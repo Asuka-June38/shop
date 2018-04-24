8c9f08af0a93775b4e8f5a5c51adc23bdd36ae49
Asuka-June38 <cbnb2012>
2018-04-24 18:17:00 +0700

    Creating sections it the list.

---
+++ app/src/main/java/asuka_june/shop/Header.java
@@ -0,0 +1,8 @@
+package asuka_june.shop;
+
+public class Header extends Merchandise {
+    public Header(String name){
+        super(name, null, null, null);
+    }
+}
+
--- app/src/main/java/asuka_june/shop/MainActivity.java
+++ app/src/main/java/asuka_june/shop/MainActivity.java
@@ -10,6 +10,10 @@ import android.widget.ListView;
 import android.widget.SimpleAdapter;
 import java.util.ArrayList;
 import java.util.HashMap;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ListView;
+
 
 public class MainActivity extends ListActivity {
     @Override
@@ -17,9 +21,31 @@ public class MainActivity extends ListActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
+
+
+        // Getting, sorting and preparing list of items for being viewed.
+        final ArrayList<Merchandise> ItemList = new MerchandiseList();
+        ArrayList<Merchandise> bookList = new ArrayList<Merchandise>();
+        ArrayList<Merchandise> discList = new ArrayList<Merchandise>();
+        for (Merchandise item : ItemList){
+            if (item.subname == "Music" ||  item.subname == "Soft" || item.subname == "Video"){
+                discList.add(item);
+            }
+            else{
+                bookList.add(item);
+            }
+        }
+        ItemList.clear();
+        ItemList.add(new Header("Books"));
+        ItemList.addAll(bookList);
+        ItemList.add(new Header("Discs"));
+        ItemList.addAll(discList);
+
+
+
         // Linking data with logic and layout.
         ArrayList<HashMap<String, String>> itemList = new ArrayList<>();
-        final ArrayList<Merchandise> ItemList = new MerchandiseList();
         for (Merchandise item: ItemList) {
             HashMap<String, String> map = new HashMap<>();
             map.put("name", item.name);
@@ -32,17 +58,22 @@ public class MainActivity extends ListActivity {
                 new String[] {"name", "subname"},
                 new int[] {android.R.id.text1, android.R.id.text2});
 
-        // Transport object from one activity to another.
+
+
         ListView listView = getListView();
         listView.setAdapter(adapter);
+
+        // Transport object from one activity to another.
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View itemClicked, int position,
                                     long id) {
                Merchandise itemToView = ItemList.get((int)id);
-               Intent intent = new Intent(getApplication(), InformationActivity.class);
-               intent.putExtra(InformationActivity.EXTRA_ITEM, (Parcelable) itemToView);
-               startActivity(intent);
+                if (itemToView.name != "Books" && itemToView.name != "Discs"){
+                   Intent intent = new Intent(getApplication(), InformationActivity.class);
+                   intent.putExtra(InformationActivity.EXTRA_ITEM, (Parcelable) itemToView);
+                   startActivity(intent);
+               }
             }
         });
     }
